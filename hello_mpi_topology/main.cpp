5db3275f2be67793ef760c0d4db527c3506f8897
Bradley Morgan <bradley@eightbitapps.com>
2020-02-04 21:47:48 -0600

    initial commit

--- hello_mpi_topology/main.cpp
+++ hello_mpi_topology/main.cpp
@@ -7,9 +7,77 @@
 //
 
 #include <iostream>
+#include <mpi.h>
+#include <array>
+#include <cmath>
+#include <sys/time.h>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+const static unsigned int DIM = 2;
+
+struct individual {
+
+    std::array<double, DIM> x;
+    
+    double result;
+    double fitness;
+    
+};
+
+double drand(double min, double max) {
+    
+    double f = (double)rand() / RAND_MAX;
+    
+    return min + f * (max - min);
+    
+}
+
+double rastrigin(std::array<double, DIM> x) {
+    
+    double sum = 10 * DIM;
+    
+    for (unsigned int i = 0; i < DIM; i++) {
+        sum += (std::pow(x[i],2) - (10 * std::cos(2 * M_PI * x[i])));
+    }
+    
+    return sum;
+}
+
+std::vector<individual> initial_population() {
+    
+    std::vector<individual> population;
+    
+    for(int i=0; i<=20; i++) {
+        
+        individual p;
+        
+        for (unsigned int i = 0; i < DIM; i++) {
+            p.x[i] = drand(-5.12, 5.12);
+        }
+        
+        p.result = rastrigin(p.x);
+        
+        population.push_back(p);
+        
+    }
+    
+    return population;
+    
+}
+
+
+int main(int argc, char * argv[]) {
+    
+    timeval time;
+    gettimeofday(&time, NULL);
+    srand((unsigned int)(time.tv_sec * 1000) + time.tv_usec);
+    
+    std::vector<individual> population = initial_population();
+    
+    for(std::vector<individual>::iterator it = population.begin(); it != population.end(); ++it) {
+        
+        printf("%f\r\n", it->result);
+        
+    }
+    
 }
