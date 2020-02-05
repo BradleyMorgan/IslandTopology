7bbf954e40006f9b35ba6d79780c9c13e5ec9d50
Bradley Morgan <bradley@eightbitapps.com>
2020-02-04 18:17:12 -0600

    Initial Commit

---
+++ hello_mpi_topology/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hello_mpi_topology
+//
+//  Created by Ian Morgan on 2/4/20.
+//  Copyright © 2020 Eight Bit Software Co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
